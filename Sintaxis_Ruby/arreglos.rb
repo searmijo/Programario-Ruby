7e7201a754921b14d68884a48ff9225299994dd8
Sergio Armijo <linkarmijo@gmail.com>
2016-12-22 12:36:31 -0600

    Add files via upload

--- Sintaxis_Ruby/arreglos.rb
+++ Sintaxis_Ruby/arreglos.rb
@@ -5,13 +5,13 @@ arreglo_dos = Array.new(5)
 puts arreglo
 puts arreglo_dos
 
-#Muestra lo que hay en la pocision 1
+#Muestra lo que hay en la posición 1
 puts arreglo[1]
 
-#Agregar un elemento en una posicion
+#Agregar un elemento en una posición
 arreglo[3] = 'agrego esta cadena en la posición 3'
 
-#Agregar elementos en la declaracion
+#Agregar elementos en la declaración
 arreglo_tres = %w[1 40 'cadena'] 
 
 #Agregar los elementos que hay a la derecha en el arreglo 
